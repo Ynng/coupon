ada6f9e06f1392617c3bdef00ee49bbca261fa0b
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2019-05-10 11:07:14 -0400

    tried to add gmail

--- spreadsheet.py
+++ spreadsheet.py
@@ -70,7 +70,15 @@ while True:
 
             if managementMode == '4':
                 pp.pprint(coupon)
-                
+
+            # if managementMode == '5':
+            #     s = smtplib.SMTP('smtp.gmail.com', 587) 
+            #     s.starttls() 
+            #     s.login("sender_email_id", "sender_email_id_password") 
+            #     message = "Message_you_need_to_send"
+            #     s.sendmail("sender_email_id", "receiver_email_id", message) 
+            #     s.quit()
+
             if managementMode == 'quit':
                 break
 
